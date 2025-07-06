158e89ff0b5e14ad74ebba6e7f8e429140f479aa
Uzair Shahid <uzairshahid7111@gmail.com>
2025-07-06 17:52:24 +0100

    Update GenderizeAPIResources.java

--- RestAssured-Cucumber-Allure-API-Automation-Java-Project/src/test/java/Genderize/GenderizeAPIResources.java
+++ RestAssured-Cucumber-Allure-API-Automation-Java-Project/src/test/java/Genderize/GenderizeAPIResources.java
@@ -1,9 +1,6 @@
 package Genderize;
 
 public interface GenderizeAPIResources {
-    String CREATE_PRODUCT = "/medicines/createProduct";
-    String UPDATE_PRODUCT = "/medicines/updateProduct";
-    String USER_LOGIN = "/medicines/login";
 
     String GET_USER_BY_NAME = "?name=%s";
     String GET_USER_BY_NAME_AND_COUNTRY = "?name=%s&country_id=%s";
